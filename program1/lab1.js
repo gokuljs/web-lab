466c20926bb9a6c3962c48a5b62e1a01dbf06c2d
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-11-24 11:54:04 +0530

    .

--- program1/lab1.js
+++ program1/lab1.js
@@ -1,12 +1,24 @@
 var screen = document.getElementById("screen");
 var buttons = document.querySelectorAll("button");
-// The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
-// here it writtens all the buttons 
-// now we have to loop through all the buttons using for of loop
+var screenvalue = ""
+    // The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
+    // here it writtens all the buttons 
+    // now we have to loop through all the buttons using for of loop
 for (item of buttons) {
     // now create an eventlistener for all the buttons we 
     item.addEventListener('click', (e) => {
-        var buttontext = e.target.innertext
+        // creating an eventlistner click for the buttons
+        // whenever we click on those buttons we are trying to get innertext of those buttons
+        var buttontext = e.target.innerText
         console.log(buttontext)
-    })
+        if (buttontext == "clear") {
+            screenvalue = ""
+            screen.value = screenvalue;
+        } else if (buttontext == "=") {
+            screen.value = eval(screenvalue);
+        } else {
+            screenvalue += buttontext;
+            screen.value = screenvalue;
+        }
+    });
 }
