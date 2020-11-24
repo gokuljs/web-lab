ffb7657285ba5eb97987a1875969cc22faab9227
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-11-24 11:05:05 +0530

    .

--- program1/lab1.js
+++ program1/lab1.js
@@ -1,10 +1,12 @@
-console.log("Getting started at javascreipt part")
-let screen = document.getElementById('screen');
-let buttons = document.querySelectorAll('button');
+var screen = document.getElementById("screen");
+var buttons = document.querySelectorAll("button");
+// The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
+// here it writtens all the buttons 
+// now we have to loop through all the buttons using for of loop
 for (item of buttons) {
-    item.addEventListener("click", (e) => {
-        buttonText = e.target.innerText
-        console.log(buttonText)
-
+    // now create an eventlistener for all the buttons we 
+    item.addEventListener('click', (e) => {
+        var buttontext = e.target.innertext
+        console.log(buttontext)
     })
 }
