4d9bdf0900c31ce19044c1931cc085916943560d
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-12-13 19:30:20 +0530

    .

--- program2/lab2.js
+++ program2/lab2.js
@@ -1,1 +1,12 @@
 alert("welcome to program 2")
+range = prompt("enter your range");
+console.log("hello world");
+console.log(range);
+if (range == ' ') {
+    alert("invalid number");
+} else {
+    document.write("<table> <th>Number</th><th>square</th><th>cube</th>");
+    for (var i = 0; i <= range; i++) {
+        document.write("<tr><td>" + i + "</td><td>" + i * i + "</td><td>" + i * i * i + "</td><tr>");
+    }
+}
