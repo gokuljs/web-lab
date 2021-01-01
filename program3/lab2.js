280ae92f08f11889fa951f13c7dd73bd0cc5c64b
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-01-01 13:42:00 +0530

    lab3

--- program3/lab2.js
+++ program3/lab2.js
@@ -1,1 +1,27 @@
 console.log("lab3 three program for text shrinking and text ")
+var message = document.getElementById("test")
+console.log("content ot be inceased and shrinked", message.innerText)
+var var1 = setInterval(increasing, 1000)
+    // setinterval attribute repeatedly calls the fucntion 
+var starting = 5;
+var ending = 50;
+
+function increasing() {
+    message.setAttribute('style', 'font-size:' + starting + "px; color:red;")
+    starting = starting + 5;
+    if (starting > ending) {
+        clearInterval(var1);
+        var2 = setInterval(decreasing, 1000)
+
+    }
+}
+
+function decreasing() {
+    ending = ending - 5;
+    message.setAttribute('style', 'font-size:' + ending + 'px; color:blue;');
+    if (ending === 5) {
+        clearInterval(var2);
+        var var1 = setInterval(increasing, 1000)
+
+    }
+}
