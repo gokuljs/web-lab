45b0e3121a2420cbf5d8a5230d4fb425c6e5fee3
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-01-24 10:47:52 +0530

    .

---
+++ practice/lab1.js
@@ -0,0 +1,19 @@
+var screen = document.getElementById("screen");
+console.log(screen);
+var buttons = document.querySelectorAll("button");
+console.log(buttons);
+var screentext = "";
+for (item of buttons) {
+    item.addEventListener('click', function(e) {
+        k = e.target.innerText
+        if (k === "=") {
+            screen.value = eval(screentext)
+        } else if (k === "clear") {
+            screentext = "";
+            screen.value = screentext
+        } else {
+            screentext = screentext + k
+            screen.value = screentext
+        }
+    });
+}
